8d2fca5af8c6cf7a87aad8250e931a48226161ae
Luca Rhee <lucarhee@gmail.com>
2021-02-23 16:34:13 +0900

    0부터 9까지 임의의 숫자 3개를 조합하여 출력함

---
+++ play-baseball-cli.py
@@ -0,0 +1,8 @@
+# searching: python generate random number
+from random import randint
+
+number1 = randint(0, 9)
+number2 = randint(0, 9)
+number3 = randint(0, 9)
+
+print("{}{}{}".format(number1, number2, number3))
