6049644fc039b1709e73b212635ca09ea448e7fd
Luca Rhee <lucarhee@gmail.com>
2021-02-23 16:39:38 +0900

    3개의 숫자가 각각 다르게 나오도록 만듬

--- play-baseball-cli.py
+++ play-baseball-cli.py
@@ -1,8 +1,26 @@
+# 1. 임의의 숫자 3개를 얻는다. 0부터 9까지의 정수이다.
 # searching: python generate random number
+
+# 2. 이 3개의 숫자는 같은 숫자면 안 된다.
+
 from random import randint
 
 number1 = randint(0, 9)
-number2 = randint(0, 9)
-number3 = randint(0, 9)
+
+while True:
+    number2 = randint(0, 9)
+
+    if number2 == number1:
+        continue
+    else:
+        break
+
+while True:
+    number3 = randint(0, 9)
+
+    if number3 == number1 or number3 == number2:
+        continue
+    else:
+        break
 
 print("{}{}{}".format(number1, number2, number3))
